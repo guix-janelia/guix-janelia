0be522ddbc8adf5ca92fdd99cd2cbca53559ddea
Peter Polidoro <peter@polidoro.io>
2022-02-20 20:31:52 -0500

    Update kicad_netlist_reader to 5.1.12

--- guix-janelia/packages/python.scm
+++ guix-janelia/packages/python.scm
@@ -1,27 +1,34 @@
 (define-module (guix-janelia packages python)
-  #:use-module (guix packages)
   #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix download)
-  #:use-module (guix build-system python)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages python)
   #:use-module (gnu packages python-build)
+  #:use-module (gnu packages python-check)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix utils)
+  #:use-module (guix build-system python)
   )
 
 
 (define python-kicad-netlist-reader
   (package
-    (name "python-kicad-netlist-reader")
-    (version "5.1.11")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (pypi-uri "kicad-netlist-reader" version))
-        (sha256
-          (base32 "13rff9gaij42x5mb5qlj54gpbbsgmr47mn74wrzajmxjkss6lj13"))))
-    (build-system python-build-system)
-    (home-page "https://github.com/janelia-pypi/kicad_netlist_reader")
-    (synopsis "KiCad python module for interpreting generic netlists.")
-    (description "KiCad python module for interpreting generic netlists.")
-    (license license:gpl3)))
+   (name "python-kicad-netlist-reader")
+   (version "5.1.12")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (pypi-uri "kicad_netlist_reader" version))
+     (sha256
+      (base32 "0hd8qz4yysy7w4pdpf7rfp2r1kwrx6hi8qnpbnzl885s00p12c8z"))))
+   (build-system python-build-system)
+   (home-page "https://github.com/janelia-pypi/kicad_netlist_reader")
+   (synopsis "KiCad python module for interpreting generic netlists.")
+   (description "KiCad python module for interpreting generic netlists.")
+   (license license:gpl3))
+  )
 
 (define-public python-kicad-bom
   (package
