e2902f77ae95e40014db18d56539929431b254eb
Peter Polidoro <peter@polidoro.io>
2022-07-18 15:06:51 -0400

    gnu: Add emacs-dirvish.
    * guix-janelia/packages/emacs-xyz.scm (emacs-dirvish): New variable.

--- guix-janelia/packages/emacs-xyz.scm
+++ guix-janelia/packages/emacs-xyz.scm
@@ -2,5 +2,41 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix git-download)
-  #:use-module (guix build-system emacs))
+  #:use-module (guix build-system emacs)
+  #:use-module (gnu packages emacs-xyz))
 
+(define-public emacs-dirvish
+  (package
+    (name "emacs-dirvish")
+    (version "1.9.23")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/alexluigit/dirvish.git")
+                    (commit "af5a099bf167729448a9b834ff9b2ec618617519")))
+              (sha256
+               (base32
+                "0s0rkpfpkx3nafahy0b96gjfsl5h1hccrx4iwqzsafm5v8q4yphr"))))
+    (build-system emacs-build-system)
+    (propagated-inputs (list emacs-transient))
+    (arguments
+     '(#:include '("^[^/]+.el$" "^[^/]+.el.in$"
+                   "^dir$"
+                   "^[^/]+.info$"
+                   "^[^/]+.texi$"
+                   "^[^/]+.texinfo$"
+                   "^doc/dir$"
+                   "^doc/[^/]+.info$"
+                   "^doc/[^/]+.texi$"
+                   "^doc/[^/]+.texinfo$"
+                   "^extensions/[^/]+.el$")
+       #:exclude '("^.dir-locals.el$" "^test.el$" "^tests.el$"
+                   "^[^/]+-test.el$" "^[^/]+-tests.el$")))
+    (home-page "https://github.com/alexluigit/dirvish")
+    (synopsis "Emacs file manager based on dired mode")
+    (description
+     "This package is an improved version of the Emacs inbuilt package Dired.
+It provides multiple window layouts, always available file preview, isolated
+sessions, and a composable user interface.")
+    (license license:gpl3+)))
+emacs-dirvish
