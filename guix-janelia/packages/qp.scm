aeb8532b3a77f038a23417e6c0c2c762e8fa9029
Peter Polidoro <peter@polidoro.io>
2025-05-21 14:28:02 -0400

    First attempt at packaging qm

--- guix-janelia/packages/qp.scm
+++ guix-janelia/packages/qp.scm
@@ -4,8 +4,12 @@
   #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix build-system copy)
+  #:use-module (guix download)
   #:use-module (gnu packages base)
   #:use-module (gnu packages commencement)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages qt)
   )
 
 (define-public qpc
@@ -43,3 +47,39 @@
     (synopsis "Real-time framework in C for building embedded software as systems of asynchronous event-driven active objects")
     (description "This package contains a real-time framework in C for building embedded systems based on active objects and hierarchical state machines.")
     (license license:gpl3+)))
+
+(define-public qm
+  (package
+    (name "qm")
+    (version "5.1.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://github.com/QuantumLeaps/qm/releases/download/v" version "/qm_" version "-linux64.zip"))
+       (sha256
+        (base32 "1jic00wfyy9if9r4z9zr4hxghm824wcfl16hii9zd6phbgw4kkmj"))))
+    (build-system copy-build-system)
+    ;; (arguments
+    ;;  (list
+    ;;   #:install-plan
+    ;;   #~'(("." "share/qpc"
+    ;;        #:exclude-regexp ("3rd_party"
+    ;;                          ".gitignore"
+    ;;                          ".gitmodules"
+    ;;                          "examples")))
+    ;;   #:modules '(((guix build gnu-build-system) #:prefix gnu:)
+    ;;               (guix build copy-build-system)
+    ;;               (guix build utils)
+    ;;               (ice-9 match))))
+    ;; (native-search-paths
+    ;;  (list (search-path-specification
+    ;;         (variable "QPC")
+    ;;         (files '("share/qpc")))))
+    (native-inputs
+     (list unzip))
+    (inputs (list coreutils gcc gcc-toolchain qtbase))
+    (home-page "https://www.state-machine.com/qm/")
+    (synopsis "")
+    (description "")
+    (license license:gpl3+)))
+qm
